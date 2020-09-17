4e8cb678b006d66cf2721f3d8a7daa31a3930df7
Eva <evaliebmann@gmx,at>
2020-09-17 17:40:06 +0200

    done

---
+++ Calculating.java
@@ -0,0 +1,43 @@
+public class Calculating {
+	
+	public static void main (String[] args) {
+		
+		byte a = 4;
+		byte b = 7; 
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(b%a);
+		
+		float c = 4.3f;
+		float d = 7; 
+		
+		System.out.println(c+d);
+		System.out.println(c-d);
+		System.out.println(c*d);
+		System.out.println(c/d);
+		System.out.println(c%d);
+		System.out.println(d%c);
+		
+		String cow = "yellow";
+		String monkey = "brown";
+		System.out.println(cow==monkey);
+		System.out.println(cow!=monkey);
+		
+		int e = 8;
+		int f = 9; 
+		
+		System.out.println(e<f);
+		System.out.println(e>f);
+		System.out.println(e>=f);
+		System.out.println(e<=f);
+		
+		
+		
+	
+	}
+	
+}
